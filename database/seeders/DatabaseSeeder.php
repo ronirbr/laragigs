f6efe41f504ebe18a4b9c42bcc46a145367250a5
ronirbr <roni.ribeiro.n00@gmail.com>
2023-01-16 12:43:33 -0300

    creating a factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,24 +17,26 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(5)->create();
 
-        Listing::create([
-          'title' => 'Laravel Senior Developer',
-          'tags' => 'laravel, javascript',
-          'company' => 'Acme Corp',
-          'location' => 'Boston, MA',
-          'email' => 'email1@email.com',
-          'website' => 'https://www.acme.com',
-          'description' => 'Beatae reiciendis natus cum nostrum fugit. Nisi nobis expedita voluptatibus culpa nisi!'
-        ]);
+        Listing::factory(6)->create();
 
-        Listing::create([
-          'title' => 'Full-Stack Engineer',
-          'tags' => 'laravel, backend, api',
-          'company' => 'Stark Industries',
-          'location' => 'New York, NY',
-          'email' => 'email2@email.com',
-          'website' => 'https://www.starkindustries.com',
-          'description' => 'Lorem optio nesciunt quaerat nostrum nostrum quasi.'
-        ]);
+        // Listing::create([
+          // 'title' => 'Laravel Senior Developer',
+          // 'tags' => 'laravel, javascript',
+          // 'company' => 'Acme Corp',
+          // 'location' => 'Boston, MA',
+          // 'email' => 'email1@email.com',
+          // 'website' => 'https://www.acme.com',
+          // 'description' => 'Beatae reiciendis natus cum nostrum fugit. Nisi nobis expedita voluptatibus culpa nisi!'
+        // ]);
+
+        // Listing::create([
+          // 'title' => 'Full-Stack Engineer',
+          // 'tags' => 'laravel, backend, api',
+          // 'company' => 'Stark Industries',
+          // 'location' => 'New York, NY',
+          // 'email' => 'email2@email.com',
+          // 'website' => 'https://www.starkindustries.com',
+          // 'description' => 'Lorem optio nesciunt quaerat nostrum nostrum quasi.'
+        // ]);
     }
 }
