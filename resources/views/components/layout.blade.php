c97c326f7120259aa863d048ecf4b0d9ff22abc7
ronirbr <roni.ribeiro.n00@gmail.com>
2023-01-29 16:16:27 -0300

    user registration

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,10 +26,10 @@
             <a href="/public/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"/></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 <li>
-                    <a href="register.html" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i>Register</a>
+                    <a href="/public/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
                 </li>
                 <li>
-                    <a href="login.html" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>Login</a>
+                    <a href="/public/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
                 </li>
             </ul>
         </nav>
