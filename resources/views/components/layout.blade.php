9515178c0e23583d5b30b854b5d5a475cd031643
ronirbr <roni.ribeiro.n00@gmail.com>
2023-01-30 09:02:56 -0300

    auth links

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,12 +25,21 @@
         <nav class="flex justify-between items-center mb-4">
             <a href="/public/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"/></a>
             <ul class="flex space-x-6 mr-6 text-lg">
+                @auth
+                <li>
+                    <span class="font-bold uppercase">Welcome {{auth()->user()->name}}</span>
+                </li>
+                <li>
+                    <a href="/public/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Listings</a>
+                </li>
+                @else
                 <li>
                     <a href="/public/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
                 </li>
                 <li>
                     <a href="/public/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
                 </li>
+                @endauth
             </ul>
         </nav>
 
