4192a1ce98cfd03e35e8f48449c40f9817a931ee
ronirbr <roni.ribeiro.n00@gmail.com>
2023-02-05 13:36:42 -0300

    user authorization

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -57,6 +57,11 @@ class ListingController extends Controller
 
     // Update Listing Data
     public function update(Request $request, Listing $listing) {
+        // Make sure logged in user is owner
+        if($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
         $formFields = $request->validate([
             'title' => 'required',
             'company' => ['required'],
@@ -78,6 +83,11 @@ class ListingController extends Controller
 
     // Delete Listing
     public function destroy(Listing $listing) {
+        // Make sure logged in user is owner
+        if($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
         $listing->delete();
         return redirect('/')->with('message', 'Listing deleted successfully');
     }
